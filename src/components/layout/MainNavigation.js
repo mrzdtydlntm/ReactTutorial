7ed75f410b9ec7cb04af1cc28e9eb483e04ab7ca
mrzdtydlntm <mrzdtydlntm@gmail.com>
2022-05-25 18:23:16 +0700

    chore: init

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import classes from "./MainNavigation.module.css"
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link> {/*  It will render like anchor tag but it will prevent browser send request but just parse url  */}
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetups</Link> {/*  It will render like anchor tag but it will prevent browser send request but just parse url  */}
+          </li>
+          <li>
+            <Link to="/favorites">Favorites</Link> {/*  It will render like anchor tag but it will prevent browser send request but just parse url  */}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
